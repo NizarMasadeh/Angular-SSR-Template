436fee91c3d2bad2500f9fe212c54325aa234ca9
Nizar Masadeh <nizarmasadeh2001@gmail.com>
2025-04-28 10:09:50 +0300

    Main layout and UI

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,14 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { ThemeToggleComponent } from '../../components/theme-toggle/theme-toggle.component';
 
 @Component({
   selector: 'app-header',
-  standalone: true,
   imports: [
     CommonModule,
-    FormsModule,
     ThemeToggleComponent
   ],
   templateUrl: './header.component.html',
